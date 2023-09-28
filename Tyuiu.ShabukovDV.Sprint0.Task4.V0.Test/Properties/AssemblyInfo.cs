c4fe75e96889471201ebb85fbd654fafbec52588
Данил Шабуков <danilshabukov8@gmail.com>
2023-09-28 19:23:50 +0400

    Task4 тоже готов :)

---
+++ Tyuiu.ShabukovDV.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShabukovDV.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShabukovDV.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1183878-99c0-4def-b79d-b7d3de56c952")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShabukovDV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShabukovDV.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.ShabukovDV.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры программы
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(100, 50));
+            //Вызов метода вычитания Subtraction
+            Console.WriteLine(DataService.Subtraction(20, 2));
+            //Вызов метода умножения Multiplication
+            Console.WriteLine(DataService.Multiplication(15, 16));
+            // Вызов метода деления Division
+            Console.WriteLine(DataService.Division(50, 10));
+
+            Console.ReadKey();
+        }
+    }
+}
